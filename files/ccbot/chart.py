a39183731c8c68fa7a237c1bfa1b99e6102e07ee
atsumarukun <atys.smz@gmail.com>
2022-06-09 19:00:18 +0900

    chart multiple support

--- files/ccbot/chart.py
+++ files/ccbot/chart.py
@@ -1,22 +1,25 @@
 import matplotlib.pyplot as plt
 
 class Chart:
-    def __init__(self, c):
-        self.c = c
-        self.asks = c.asks
+    def __init__(self, *args):
         self.fig, self.ax = plt.subplots()
         self.fig.canvas.draw()
         self.bg = self.fig.canvas.copy_from_bbox(self.ax.bbox)
-        self.data = [0 for _ in range(100)]
-        self.line, = self.ax.plot(self.data)
+
+        self.coms = args
+
+        for com in self.coms:
+            com.top_asks = [0 for _ in range(100)]
+            com.line, = self.ax.plot(com.top_asks)
         self.fig.show()
 
     async def draw_chart(self):
-        self.data.append(self.asks.values()[0][0])
-        del self.data[0]
-        print(len(self.data))
-        self.line.set_ydata(self.data)
         self.fig.canvas.restore_region(self.bg)
-        self.ax.draw_artist(self.line)
+        for com in self.coms:
+            com.top_asks.append(com.asks.values()[0][0])
+            del com.top_asks[0]
+            com.line.set_ydata(com.top_asks)
+            self.ax.draw_artist(com.line)
+       
         self.fig.canvas.blit(self.ax.bbox)
         self.fig.canvas.flush_events()
--- files/ccbot/ws.py
+++ files/ccbot/ws.py
@@ -2,11 +2,11 @@ import asyncio
 import aiohttp
 
 async def ws_run_forever(session: aiohttp.ClientSession, chart) -> None:
-    async with session.ws_connect(chart.c.WEBSOCKET_URL) as ws:
-        await ws.send_json(chart.c.params)
+    async with session.ws_connect(chart.coms[0].WEBSOCKET_URL) as ws:
+        await ws.send_json(chart.coms[0].params)
         async for msg in ws:
             if msg.type == aiohttp.WSMsgType.TEXT:
-                chart.c.orderbook(msg.json())
+                chart.coms[0].orderbook(msg.json())
                 await chart.draw_chart()
             elif msg.type == aiohttp.WSMsgType.ERROR:
                 print(msg.json())
